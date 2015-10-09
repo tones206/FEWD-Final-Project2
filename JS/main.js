eb4560c7572ab89d91818501818b204c7fad4702
tones206 <Tones@Antonias-MacBook-Pro.local>
2015-10-08 18:18:24 -0700

    Final

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,26 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-$('#slideshow').cycle({
-	fx: 'fade',
-	pager: '#smallnav',
-	pause:   1,
-	speed: 1800,
-	timeout:  3500
+	$('#slideshow').cycle({
+		fx: 'fade',
+		pager: '#smallnav',
+		pause:   1,
+		speed: 1800,
+		timeout:  3500
 	});
-});
 
 
-$("#navigation a").click(function() {
-  var section = $(this).attr('href');
-  $('html, body').animate({
-      scrollTop: $(section).offset().top
-  }, 1000);
+	$("#navigation a").click(function() {
+	  var section = $(this).attr('href');
+	  $('html, body').animate({
+	      scrollTop: $(section).offset().top
+	  }, 1000);
+	});
+
+	$("footer a").click(function() {
+	  var section = $(this).attr('href');
+	  $('html, body').animate({
+	      scrollTop: $(section).offset().top
+	  }, 1000);
+	});
 });
